f6c4713abf8049a65eb93a6ea0dcd1634e41deb8
bhaskerrai <bhaskerrai.dev@gmail.com>
2024-02-28 15:29:04 +0500

    Completed till stack

--- 01_Introduction/recursion.py
+++ 01_Introduction/recursion.py
@@ -26,3 +26,16 @@ def fibonacciNumber(n):
     return oneBack + twoBack
 
 print(fibonacciNumber(4))
+
+
+def factorial(i: int):
+    if i < 0:
+        raise ValueError("Factorial is not defined for negative numbers")
+
+    if i == 0 or i == 1:
+        return 1
+        
+    # print(i)
+    return i * factorial(i - 1)
+
+# print(factorial(3))
